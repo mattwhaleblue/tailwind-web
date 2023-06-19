8fd43c8cf5cb4c8dc89986e9ea7913b566fd008f
Matt Hardman <matt@whaleblue.me>
2023-06-19 15:06:48 +0100

    asd

--- backend/api/send.js
+++ backend/api/send.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend('RESEND_API_KEY');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(request, response) {
   try {
